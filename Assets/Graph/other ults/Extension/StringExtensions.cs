a3659d9eb610a6fed8f42e3e60990532ec34f679
NDDEVVlog <111946313+NDDEVVlog@users.noreply.github.com>
2024-02-01 21:11:30 +0700

    Delete Assets/Graph directory

--- Assets/Graph/other ults/Extension/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace cmdwtf.UnityTools.Editor
-{
-	public static class StringExtensions
-	{
-		public static GUIContent ToContent(this string s) => new GUIContent(s, s);
-	}
-}
